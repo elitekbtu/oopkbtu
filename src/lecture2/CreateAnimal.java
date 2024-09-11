5e6e15186653fe32ec1e003a18b01288ea4f09fe
elitekbtu <satbaldievst@mail.ru>
2024-09-11 17:15:17 +0500

    lecture2

--- src/lecture2/CreateAnimal.java
+++ src/lecture2/CreateAnimal.java
@@ -2,8 +2,8 @@ package lecture2;
 
 public class CreateAnimal {
     public static void main(String[] args) {
-//      Animal animal = new Animal("Hatiko", 100, "Male", 1);
-//      animal.setName("Kasparov");
+      Animal animal = new Animal("Hatiko", 100, "Male", 1);
+      animal.setName("Kasparov");
         Animal.age++;
         System.out.println(Animal.age);
     }
