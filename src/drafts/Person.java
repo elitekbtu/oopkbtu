bf21597e452a2c7099e4cb4f84ecd5e5e357af6e
elitekbtu <satbaldievst@mail.ru>
2024-09-16 14:05:08 +0500

    lecture

---
+++ src/drafts/Person.java
@@ -0,0 +1,10 @@
+package drafts;
+
+public class Person{
+
+    String name;        // имя
+    int age;            // возраст
+    void displayInfo(){
+        System.out.printf("Name: %s \tAge: %d\n", name, age);
+    }
+}
