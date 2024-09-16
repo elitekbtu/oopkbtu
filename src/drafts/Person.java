8bba64a2412c4607a0173f9aa6448e39f8516a91
elitekbtu <satbaldievst@mail.ru>
2024-09-16 14:12:46 +0500

    drafts

--- src/drafts/Person.java
+++ src/drafts/Person.java
@@ -1,10 +1,17 @@
 package drafts;
 
-public class Person{
+public class Person {
+    String name;  // имя
+    int age;      // возраст
 
-    String name;        // имя
-    int age;            // возраст
-    void displayInfo(){
+    void displayInfo() {
         System.out.printf("Name: %s \tAge: %d\n", name, age);
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person();  // Create an object of Person class
+        person.name = "John";          // Assign values to fields
+        person.age = 30;
+        person.displayInfo();          // Call the non-static method on the object
     }
 }
