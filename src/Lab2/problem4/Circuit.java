137bc7b9a622c87e26e609a5b3b694a20fc595c3
elitekbtu <satbaldievst@mail.ru>
2024-10-13 20:16:36 +0500

    lab works

---
+++ src/Lab2/problem4/Circuit.java
@@ -0,0 +1,18 @@
+package Lab2.problem4;
+
+public abstract class Circuit {
+    public abstract double getResistance();
+
+    public abstract double getPotentialDifference();
+
+    public abstract void applyPotentialDifference(double voltage);
+
+    public double getPower() {
+        return Math.pow(getPotentialDifference(), 2) / getResistance();
+    }
+
+    public double getCurrent() {
+        return getPotentialDifference() / getResistance();
+    }
+}
+
